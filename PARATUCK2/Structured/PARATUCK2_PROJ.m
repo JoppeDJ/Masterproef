6394da4abbcbe3d895f31b3e4d9e26ab05324d31
Joppe <joppdj@gmail.com>
2022-10-31 21:13:50 +0100

    fix results

--- PARATUCK2/Structured/PARATUCK2_PROJ.m
+++ PARATUCK2/Structured/PARATUCK2_PROJ.m
@@ -5,6 +5,9 @@ function [W, D2, Vt, D1, Zt, cD1, cD2] = PARATUCK2_PROJ(Jac, bf1, bf2, r1, r2, s
     [I, J, K] = size(Jac);
     d1 = length(bf1);
     d2 = length(bf2);
+
+    lambda1 = 1;
+    lambda2 = 1;
     
     cD1 = zeros(r1*d1,1);
     cD2 = zeros(r2*d2,1);
@@ -15,26 +18,37 @@ function [W, D2, Vt, D1, Zt, cD1, cD2] = PARATUCK2_PROJ(Jac, bf1, bf2, r1, r2, s
     D1 = randn(K, r1);
     Zt = randn(r1, J);
 
+    Zt = updateZt(Jac, W, D1, Vt, D2, I, J, K, r1);
+
+    D1 = updateD1(Jac, W, Vt, Zt, D2, K, r1);
+    
+    Vt = updateVt(Jac, W, D1, D2, Zt, I, J, K, r1, r2);        
+
+    D2 = updateD2(Jac, W, Vt, Zt, D1, K, r2);
+
+    W = updateW(Jac, D2, Vt, D1, Zt, I, J, K, r2);
+
     lastError = 1e5;
     
     for i=1:100
-
-        % Projectie strategie
-        [cD2, D2] = update_cD2(D2, Vt, Zt, samples, cD1, bf1, bf2, K, r2, d2);
-        
+       
         Zt = updateZt(Jac, W, D1, Vt, D2, I, J, K, r1);
-        
-        Vt = updateVt(Jac, W, D1, D2, Zt, I, J, K, r1, r2);
-        
-        W = updateW(Jac, D2, Vt, D1, Zt, I, J, K, r2);
 
         D1 = updateD1(Jac, W, Vt, Zt, D2, K, r1);
         
         % Projectie strategie
-        [cD1, D1] = update_cD1(D1, Zt, samples, bf1, K, r1, d1);
-        
+        [cD1, D1] = update_cD1(cD1, D1, Zt, samples, bf1, K, r1, d1, lambda1);
+
+        Vt = updateVt(Jac, W, D1, D2, Zt, I, J, K, r1, r2);        
+
         D2 = updateD2(Jac, W, Vt, Zt, D1, K, r2);
         
+        % Projectie strategie
+        [cD2, D2] = update_cD2(cD2, D2, Vt, Zt, samples, cD1, bf1, bf2, K, r2, d2, lambda2);
+
+        %Vt = updateVt(Jac, W, D1, D2, Zt, I, J, K, r1, r2);
+        
+        W = updateW(Jac, D2, Vt, D1, Zt, I, J, K, r2);
     
         apprJac = zeros(I, J, K);
         for j=1:K
@@ -43,7 +57,7 @@ function [W, D2, Vt, D1, Zt, cD1, cD2] = PARATUCK2_PROJ(Jac, bf1, bf2, r1, r2, s
         end
         
         error = frob(Jac - apprJac)^2 / frob(Jac)^2
-        if(error < 0.01 || abs(error-lastError) < 0.00005)
+        if(error < 0.001 || abs(error-lastError) < 0.00005)
             break
         end
 
@@ -125,7 +139,7 @@ function [Zt] = updateZt(X, W, D1, Vt, D2, I, J, K, r1)
     Zt = F \ unfoldX; % Paper gebruikt F^T (lijkt verkeerd)
 end
 
-function [cD1, D1] = update_cD1(D1, Zt, samples, bf1, K, r1, d1)
+function [cD1, D1] = update_cD1(cD1, D1, Zt, samples, bf1, K, r1, d1, lambda1)
     % Projectiestrategie
     X = zeros(r1*K, r1*d1);
     for l=1:r1
@@ -141,14 +155,23 @@ function [cD1, D1] = update_cD1(D1, Zt, samples, bf1, K, r1, d1)
         X(((l-1)*K) + 1:l * K, ((l-1)*d1) + 1:l * d1) = Xl;
     end
 
-    cD1 = (reshape(D1, numel(D1), 1)'/ X')';
-
-    for l=1:r1
-        D1(:,l) = X(((l-1)*K) + 1:l * K, ((l-1)*d1) + 1:l * d1) * cD1((l-1) * d1 + 1: l * d1);
+    not_converged = true;
+    while not_converged
+        obj1 = frob(reshape(D1, numel(D1), 1) - X * cD1)^2 + lambda1 * frob(cD1)^2;
+        if obj1 < 100
+            not_converged = false;
+        else
+            cD1 = ...
+                pinv(X'*X + lambda1 * eye(r1*d1))*X'*reshape(D1, numel(D1), 1);
+            for l=1:r1
+                D1(:,l) = X(((l-1)*K) + 1:l * K, ((l-1)*d1) + 1:l * d1) * cD1((l-1) * d1 + 1: l * d1);
+            end
+        end
     end
+    %cD1 = (reshape(D1, numel(D1), 1)'/ X')';
 end
 
-function [cD2, D2] = update_cD2(D2, Vt, Zt, samples, cD1, bf1, bf2, K, r2, d2)
+function [cD2, D2] = update_cD2(cD2, D2, Vt, Zt, samples, cD1, bf1, bf2, K, r2, d2, lambda2)
     % Projectiestrategie
     X = zeros(r2*K, r2*d2);
     for l=1:r2
@@ -167,11 +190,21 @@ function [cD2, D2] = update_cD2(D2, Vt, Zt, samples, cD1, bf1, bf2, K, r2, d2)
         X(((l-1)*K) + 1:l * K, ((l-1)*d2) + 1:l * d2) = Xl;
     end
 
-    cD2 = (reshape(D2, numel(D2), 1)'/ X')';
-
-    for l=1:r2
-        D2(:,l) = X(((l-1)*K) + 1:l * K, ((l-1)*d2) + 1:l * d2) * cD2((l-1) * d2 + 1: l * d2);
+    not_converged = true;
+    while not_converged
+        obj2 = frob(reshape(D2, numel(D2), 1) - X * cD2)^2 + lambda2 * frob(cD2)^2;
+        if obj2 < 200
+            not_converged = false;
+        else
+            cD2 = ...
+                pinv(X'*X + lambda2 * eye(r2*d2))*X'*reshape(D2, numel(D2), 1);
+            for l=1:r2
+                D2(:,l) = X(((l-1)*K) + 1:l * K, ((l-1)*d2) + 1:l * d2) * cD2((l-1) * d2 + 1: l * d2);
+            end
+        end
     end
+
+    %cD2 = (reshape(D2, numel(D2), 1)'/ X')';
 end
 
 function [result] = applyFlexibleFunctions(inputVec, c, bf)
